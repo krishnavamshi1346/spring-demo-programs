b29169655117f4095071d0a7f862269652c211d7
Krishna Vamshi <krishnavamshi1346@gmail.com>
2021-05-23 14:13:15 +0530

    SpringBoot Projects

---
+++ 05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,41 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	//inject properties for cocah.name,team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+
+	//expose "/" that return hello world
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Hello-world Time on server is"+LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout()
+	{
+		return  "Run a hard 5k";
+	}
+	//expose "/" for team info
+
+	@GetMapping("/teaminfo")
+	public String getTeamInfo()
+	{
+		return "Coach : "+ coachName + "Team : " + teamName;
+	}
+
+}
